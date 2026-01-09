833b2500904a906b117c7d06ceffe6139704ebad
KarenUtilDecor <karenutildecor@gmail.com>
2026-01-09 18:15:09 -0300

    remoção do api key gemini

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fgywsxqettxqvjnecube.supabase.co';
-const supabaseAnonKey = 'sb_publishable_lDdJL8MxOtlUsjimjuJIDw_BCT4VkfF';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
